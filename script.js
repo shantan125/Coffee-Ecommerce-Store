9f385e17e98ed66253ce7aeda34c1260223f1e78
Yeddula Shantan <100433099+shantan125@users.noreply.github.com>
2024-02-26 22:20:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
-window.addEventListener('scroll',function(){
-    let navbar = document.querySelector('.navsec');
-    let scrollposition = window.scrollY;
-    if(scrollposition>0){
-        navbar.classList.add('scroll');
-    }
-    else{
-        navbar.classList.remove('scroll');
-    }
+window.addEventListener("load", function () {
+  new Glider(document.querySelector(".glider"), {
+    settingName: settingValue,
+  });
 });
 
 new Glider(document.querySelector(".glider"), {
@@ -42,4 +37,64 @@ new Glider(document.querySelector(".glider"), {
       },
     },
   ],
+});
+
+// Initialize Glider for .glider2
+window.addEventListener("load", function () {
+  const glider2 = new Glider(document.querySelector(".glider2"), {
+    slidesToShow: 3,
+    dots: ".dots2",
+    arrows: {
+      prev: ".glider2-prev",
+      next: ".glider2-next",
+    },
+  });
+
+  // Event listener for clicking on the previous arrow button
+  document.querySelector(".glider2-prev").addEventListener("click", function () {
+    glider2.scrollItem("prev");
+  });
+
+  // Event listener for clicking on the next arrow button
+  document.querySelector(".glider2-next").addEventListener("click", function () {
+    glider2.scrollItem("next");
+  });
+});
+
+
+/* shop section */
+// Function to show the preview section
+function showPreview(dataName) {
+  let previewSection = document.getElementById("preview-section");
+  let prvs = document.querySelectorAll(".prv-part");
+  prvs.forEach(function (prv) {
+    if (prv.getAttribute("data-name") === dataName) {
+      prv.style.display = "block";
+    } else {
+      prv.style.display = "none";
+    }
+  });
+  previewSection.classList.add("blur");
+  previewSection.style.display = "block";
+}
+
+// Function to close the preview section
+function closePreview() {
+  let previewSection = document.getElementById("preview-section");
+  let prvs = document.querySelectorAll(".prv-part");
+  prvs.forEach(function (prv) {
+    prv.style.display = "none";
+  });
+  previewSection.classList.remove("blur");
+  previewSection.style.display = "none";
+}
+new Glider(document.querySelector(".glider"), {
+  slidesToShow: 5,
+  slidesToScroll: 5,
+  draggable: true,
+  dots: ".dots",
+  arrows: {
+    prev: ".glider-prev",
+    next: ".glider-next",
+  },
 });
